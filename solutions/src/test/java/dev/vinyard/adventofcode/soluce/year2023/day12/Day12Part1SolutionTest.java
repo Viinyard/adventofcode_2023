1bd55521c7b354f291233c1ebc2bab803ac5ee26
Viinyard <viinyard@users.noreply.github.com>
2024-12-01 12:11:36 +0000

    Solution for 2023-12

---
+++ solutions/src/test/java/dev/vinyard/adventofcode/soluce/year2023/day12/Day12Part1SolutionTest.java
@@ -0,0 +1,23 @@
+package dev.vinyard.adventofcode.soluce.year2023.day12;
+
+import dev.vinyard.adventofcode.common.BaseTest;
+import dev.vinyard.aoc.plugins.solution.api.Solution;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+public class Day12Part1SolutionTest extends BaseTest<Long> {
+
+    @Override
+    public Solution<Long> getSolution() {
+        return new Day12Part1Solution();
+    }
+
+    @Override
+    public Stream<Arguments> testSolution() {
+        return Stream.of(
+            Arguments.of("soluce/year2023/day12/test/test.txt", null, null, null),
+            Arguments.of("soluce/year2023/day12/input.txt", null, null, null)
+        );
+    }
+}
